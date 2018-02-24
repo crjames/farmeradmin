94e7df1b7069328e333d8c1b29ea506542c8f13f
wd王栋 <wang.donga@Ctrip.com>
2018-02-24 18:02:53 +0800

    add swagger2

--- src/main/java/com/farmerassistant/FarmeradminApplication.java
+++ src/main/java/com/farmerassistant/FarmeradminApplication.java
@@ -1,11 +1,13 @@
 package com.farmerassistant;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.farmerassistant")
+@EnableAutoConfiguration
 public class FarmeradminApplication {
 
 	public static void main(String[] args) {
