5d5e00c53f83f85a0cfadc7db9a1c223ebf15f7c
wd王栋 <wang.donga@Ctrip.com>
2018-03-16 14:14:43 +0800

    20180316-002

--- src/main/java/com/farmerassistant/config/WebMvcConfig.java
+++ src/main/java/com/farmerassistant/config/WebMvcConfig.java
@@ -11,6 +11,6 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/index").setViewName("index");
     }
 }
--- src/main/java/com/farmerassistant/controller/HomeController.java
+++ src/main/java/com/farmerassistant/controller/HomeController.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,13 +27,12 @@ public class HomeController {
     private SysUserMapper sysUserMapper;
     @ApiOperation(value="首页", notes="")
     @RequestMapping("/index")
-    public ModelAndView index(Model model){
+    public String index(ModelMap model){
         Msg msg =  new Msg("测试标题","测试内容","额外信息，只对管理员显示");
         model.addAttribute("msg",msg);
-        ModelAndView mv = new ModelAndView("index");
-        mv.addObject("msg",msg);
-
-        return mv;
+//        ModelAndView mv = new ModelAndView("index");
+//        mv.addObject("msg",msg);
+        return "index";
     }
     @ApiOperation(value="根据用户工号查询用户信息", notes="")
     @RequestMapping(value="/{userjobNumber}",method = RequestMethod.GET)
